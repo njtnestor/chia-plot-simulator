eace65859229d3813035586e209e2af82e8dac3a
Maria Alonso <maria.alonso@digio.es>
2021-05-16 16:01:14 +0200

    Added plot log reading process

--- components/Gantt.vue
+++ components/Gantt.vue
@@ -22,20 +22,22 @@ export default {
   },
 
   mounted () {
+    this.$gantt().config.fit_tasks = true
     this.$gantt().config.round_dnd_dates = false
     this.$gantt().config.time_step = 1
-    this.$gantt().config.columns[1].width = 150
+    this.$gantt().config.min_column_width = 100
+    this.$gantt().config.grid_width = 500
     this.$gantt().config.duration_unit = 'minute'
-    this.$gantt().config.date_grid = '%Y-%m-%d %Hh:%i'
+    this.$gantt().config.date_grid = '%Y-%m-%d %H:%i'
 
     this.$gantt().config.scales = [
       { unit: 'hour', step: 1, format: '%Hh' },
-      { unit: 'day', step: 1, format: '%j, %D' }
+      { unit: 'day', step: 1, format: '%Y-%m-%d, %D' }
 
     ]
     this.$gantt().config.columns = [
-      { name: 'text', label: 'Task name', width: '*', tree: true },
-      { name: 'start_date', label: 'Start time', align: 'center' },
+      // { name: 'text', label: 'Task name', width: '*', tree: true },
+      { name: 'start_date', label: 'Start time', align: 'center', width: 150 },
       {
         name: 'duration',
         label: 'Duration',
@@ -44,10 +46,21 @@ export default {
           return (obj.totalTime) ? new Date(Number(obj.totalTime) * 1000).toISOString().substr(11, 8) : ''
         }
       },
-      { name: 'threads', label: 'Threads', align: 'center' },
+      { name: 'threads', label: 'Threads', align: 'center', width: 50 },
       { name: 'ram', label: 'Ram', align: 'center' },
-      { name: 'size', label: 'Size', align: 'center' }
+      { name: 'size', label: 'Size', align: 'center', width: 50 },
+      { name: 'add', label: '', width: 44 }
     ]
+
+    this.$gantt().plugins({
+      tooltip: true
+    })
+    this.$gantt().templates.tooltip_text = function (start, end, task) {
+      return `<b>Phase:</b> ${task.text}
+              <br/><b>Duration:</b> ${new Date(Number(task.totalTime) * 1000).toISOString().substr(11, 8)}
+              <br/><b>Start date:</b> ${start.toLocaleString()}
+              <br/><b>End date:</b> ${end.toLocaleString()}`
+    }
     this.$gantt().init(this.$refs.gantt)
     this.$gantt().parse(this.$props.tasks)
     /* gantt.init(this.$refs.gantt)
--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,6 @@
           Selected file: {{ file ? file.name : '' }}
         </div>
 
-        <input v-model="tasks.data[1].start_date" @input="change">
         <b-button @click="send">
           click
         </b-button>
@@ -40,128 +39,124 @@ export default {
       plotName: undefined,
       file: undefined,
       newPlot: false,
+      plots: [],
       tasks: {
-        data: [
-          {
-            id: '1',
-            text: 'Q1 - P1',
-            start_date: '01-04-2021 14:10',
-            end_date: '01-04-2021 21:54',
-            totalTime: 26052.671,
-            progress: 1,
-            holder: 'hi',
-            size: `k${32}`,
-            threads: 6,
-            ram: 5495,
-            open: false,
-            render: 'split'
-          },
-          {
-            text: 'F1',
-            start_date: '01-04-2021 14:14',
-            end_date: '01-04-2021 16:37',
-            progress: 1,
-            open: false,
-            color: 'pink',
-            parent: 1
-          },
-          {
-            text: 'F2',
-            start_date: '01-04-2021 16:37',
-            end_date: '01-04-2021 18:34',
-            progress: 1,
-            color: '#35FFF0',
-            parent: 1,
-            open: false
-          },
-          {
-
-            text: 'F3',
-            start_date: '01-04-2021 18:34',
-            end_date: '01-04-2021 21:12',
-            progress: 1,
-            color: 'lime',
-            parent: 1,
-            open: false
-          },
-          {
-            text: 'F4',
-            start_date: '01-04-2021 21:12',
-            end_date: '01-04-2021 21:23',
-            progress: 1,
-            color: 'red',
-            parent: 1,
-            open: false
-          },
-          {
-            text: 'Copy',
-            start_date: '01-04-2021 21:23',
-            end_date: '01-04-2021 21:54',
-            progress: 1,
-            color: 'grey',
-            parent: 1,
-            open: false
-          }
-          /* {
-            id: '7',
-            text: 'Q2 - P1',
-            start_date: '01-04-2021 14:10',
-            end_date: '01-04-2021 21:54',
-            progress: 1,
-            open: false,
-            render: 'split'
-          } */
-        ]
-
+        data: []
       }
     }
   },
   methods: {
-    change () {
-      // https://docs.dhtmlx.com/gantt/api__gantt_refreshdata.html
-      console.log('hi')
-      this.$gantt().refreshData()
-      this.$gantt().refreshTask(2)
-      this.$gantt().render()
+    addPlotTasks (plot) {
+      const taskId = this.$gantt().addTask({
+        text: plot.id,
+        start_date: new Date(plot.phaseOne.startDate),
+        end_date: new Date(plot.copyPhase.endDate),
+        totalTime: plot.totalTime,
+        progress: 1,
+        holder: 'hi',
+        size: `k${plot.plotSize}`,
+        threads: plot.threads,
+        ram: plot.ram,
+        open: false,
+        render: 'split'
+      })
+      this.$gantt().addTask({
+        text: 'PHASE 1',
+        start_date: new Date(plot.phaseOne.startDate),
+        totalTime: plot.phaseOne.duration,
+        end_date: new Date(plot.phaseOne.endDate),
+        progress: 1,
+        open: false,
+        color: '#fe3d5e',
+        parent: taskId
+      })
+      this.$gantt().addTask({
+        text: 'PHASE 2',
+        start_date: new Date(plot.phaseTwo.startDate),
+        totalTime: plot.phaseTwo.duration,
+        end_date: new Date(plot.phaseTwo.endDate),
+        progress: 1,
+        color: '#99ff35',
+        parent: taskId,
+        open: false
+      })
+      this.$gantt().addTask({
+
+        text: 'PHASE 3',
+        start_date: new Date(plot.phaseThree.startDate),
+        totalTime: plot.phaseThree.duration,
+        end_date: new Date(plot.phaseThree.endDate),
+        progress: 1,
+        color: 'lime',
+        parent: taskId,
+        open: false
+      })
+      this.$gantt().addTask({
+        text: 'PHASE 4',
+        start_date: new Date(plot.phaseFour.startDate),
+        totalTime: plot.phaseFour.duration,
+        end_date: new Date(plot.phaseFour.endDate),
+        progress: 1,
+        color: 'red',
+        parent: taskId,
+        open: false
+      })
+      this.$gantt().addTask({
+        text: 'COPY',
+        start_date: new Date(plot.copyPhase.startDate),
+        totalTime: plot.copyPhase.duration,
+        end_date: new Date(plot.copyPhase.endDate),
+        progress: 1,
+        color: 'grey',
+        parent: taskId,
+        open: false
+      })
     },
     send () {
       // this.$gantt().parse(this.tasks)
       const reader = new FileReader()
 
-      reader.onload = function (evt) {
+      reader.onload = () => {
         // console.log(reader.result)
-        const phase1Startinfo = reader.result.split('Starting phase 1/4: Forward Propagation into tmp files... ')[1].split('\n')[0].split(' ')
-        console.log('-f1 start-date: ', phase1Startinfo[0], phase1Startinfo[1], phase1Startinfo[2], phase1Startinfo[3], phase1Startinfo[4])
-        const phase1Endinfo = reader.result.split('Time for phase 1 = ')[1].split('\n')[0].split(' ')
-        console.log('f1 end-duration: ', phase1Endinfo[0])
-        console.log('f1 end-date: ', phase1Endinfo[4], phase1Endinfo[5], phase1Endinfo[6], phase1Endinfo[7], phase1Endinfo[8])
-
-        const phase2Startinfo = reader.result.split('Starting phase 2/4: Backpropagation into tmp files... ')[1].split('\n')[0].split(' ')
-        console.log('-f2 start-date: ', phase2Startinfo[0], phase2Startinfo[1], phase2Startinfo[2], phase2Startinfo[3], phase2Startinfo[4])
-        const phase2Endinfo = reader.result.split('Time for phase 2 = ')[1].split('\n')[0].split(' ')
-        console.log('f2 end-duration: ', phase2Endinfo[0])
-        console.log('f2 end-date: ', phase2Endinfo[4], phase2Endinfo[5], phase2Endinfo[6], phase2Endinfo[7], phase2Endinfo[8])
-
-        const phase3Startinfo = reader.result.split('Starting phase 3/4: ')[1].split('\n')[0].split('... ')[1].split(' ')
-        console.log('-f3 start-date: ', phase3Startinfo[0], phase3Startinfo[1], phase3Startinfo[2], phase3Startinfo[3], phase3Startinfo[4])
-        const phase3Endinfo = reader.result.split('Time for phase 3 = ')[1].split('\n')[0].split(' ')
-        console.log('f3 end-duration: ', phase3Endinfo[0])
-        console.log('f3 end-date: ', phase3Endinfo[4], phase3Endinfo[5], phase3Endinfo[6], phase3Endinfo[7], phase3Endinfo[8])
-
-        const phase4Startinfo = reader.result.split('Starting phase 4/4: ')[1].split('\n')[0].split('... ')[1].split(' ')
-        console.log('-f4 start-date: ', phase4Startinfo[0], phase4Startinfo[1], phase4Startinfo[2], phase4Startinfo[3], phase4Startinfo[4])
-        const phase4Endinfo = reader.result.split('Time for phase 4 = ')[1].split('\n')[0].split(' ')
-        console.log('f4 end-duration: ', phase4Endinfo[0])
-        console.log('f4 end-date: ', phase4Endinfo[4], phase4Endinfo[5], phase4Endinfo[6], phase4Endinfo[7], phase4Endinfo[8])
-
-        const totalTime = reader.result.split('Total time = ')[1].split('\n')[0].split(' ')[0]
-        console.log('-Total time: ', totalTime)
-
-        const phaseCopyInfo = reader.result.split('Copy time = ')[1].split('\n')[0].split(' ')
-        console.log('-fCopy duration: ', phaseCopyInfo[0])
-        console.log('fCopy end-date: ', phaseCopyInfo[4], phaseCopyInfo[5], phaseCopyInfo[6], phaseCopyInfo[7], phaseCopyInfo[8])
+        const plot = {
+          phaseOne: {
+            startDate: reader.result.split('Starting phase 1/4: Forward Propagation into tmp files... ')[1].split('\n')[0],
+            endDate: reader.result.split('Time for phase 1 = ')[1].split('\n')[0].split(' ').slice(4).join(' '),
+            duration: reader.result.split('Time for phase 1 = ')[1].split('\n')[0].split(' ')[0]
+          },
+          phaseTwo: {
+            startDate: reader.result.split('Starting phase 2/4: Backpropagation into tmp files... ')[1].split('\n')[0],
+            endDate: reader.result.split('Time for phase 2 = ')[1].split('\n')[0].split(' ').slice(4).join(' '),
+            duration: reader.result.split('Time for phase 2 = ')[1].split('\n')[0].split(' ')[0]
+          },
+          phaseThree: {
+            startDate: reader.result.split('Starting phase 3/4: ')[1].split('\n')[0].split('... ')[1],
+            endDate: reader.result.split('Time for phase 3 = ')[1].split('\n')[0].split(' ').slice(4).join(' '),
+            duration: reader.result.split('Time for phase 3 = ')[1].split('\n')[0].split(' ')[0]
+          },
+          phaseFour: {
+            startDate: reader.result.split('Starting phase 4/4: ')[1].split('\n')[0].split('... ')[1],
+            endDate: reader.result.split('Time for phase 4 = ')[1].split('\n')[0].split(' ').slice(4).join(' '),
+            duration: reader.result.split('Time for phase 4 = ')[1].split('\n')[0].split(' ')[0]
+          },
+          copyPhase: {
+            startDate: reader.result.split('Time for phase 4 = ')[1].split('\n')[0].split(' ').slice(4).join(' '),
+            endDate: reader.result.split('Copy time = ')[1].split('\n')[0].split(' ').slice(4).join(' '),
+            duration: reader.result.split('Copy time = ')[1].split('\n')[0].split(' ')[0]
+          },
+          totalTime: reader.result.split('Total time = ')[1].split('\n')[0].split(' ')[0],
+          plotSize: reader.result.split('Plot size is: ')[1].split('\n')[0],
+          ram: reader.result.split('Buffer size is: ')[1].split('\n')[0],
+          buckets: reader.result.split('Buffer size is: ')[1].split('\n')[1].split(' ')[1],
+          threads: reader.result.split('Buffer size is: ')[1].split('\n')[2].split(' ')[1],
+          id: reader.result.split('ID: ')[1].split('\n')[0]
+        }
+        console.log(plot)
+        this.plots.push(plot)
+        this.addPlotTasks(plot)
       }
       reader.readAsText(this.file)
+      this.newPlot = false
     }
   }
 }
